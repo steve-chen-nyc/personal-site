43f2e14c3f0b3e45c99c9594f7a4b031f1ae18b3
steve-chen-nyc <steve.l.chen@gmail.com>
2020-09-02 22:23:32 -0400

    made responsive

--- components/Main.vue
+++ components/Main.vue
@@ -1,12 +1,12 @@
 <template>
   <section
-    class="bg-white h-48 flex items-center justify-between px-8 w-3/5 c-main__animate"
+    class="bg-white h-48 flex flex-col lg:flex-row items-center justify-between px-8 w-full lg:w-3/5 c-main__animate"
   >
-    <div class="w-1/2">
-      <h1 class="text-black uppercase">Steve Chen</h1>
+    <div class="mt-4 lg:m-0 lg:w-1/2">
+      <h1 class="text-center uppercase">Steve Chen</h1>
       <h5>Software Engineer</h5>
     </div>
-    <div class="w-48 transform rotate-90">
+    <div class="w-full w-full lg:w-48 transform lg:rotate-90">
       <MainLink v-for="link in links" :title="link" :key="link" />
     </div>
   </section>
